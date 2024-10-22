f7ea2c44fa1bf0682d50a4f428048d366f25f347
Shovan-777 <shovanshiwakoti7@gmail.com>
2024-10-22 17:57:33 +0545

    Updated List.jsx

--- part2/phonebook/src/components/List.jsx
+++ part2/phonebook/src/components/List.jsx
@@ -1,9 +1,10 @@
-const List = ({ persons }) => {
+const List = ({ persons, onDelete }) => {
     return (
       <ul>
         {persons.map(person => (
-          <li key={person.name}>
+          <li key={person.id}>
             {person.name} {person.num}
+            <button onClick={() => onDelete(person.id, person.name)}>Delete</button>
           </li>
         ))}
       </ul>
